f1290a2400a2f256ad51bf6c552a5bb8fc8190c5
farah-gomawi <farah.gomawi@gmail.com>
2024-09-29 14:55:56 +0300

    Update app.R

--- app.R
+++ app.R
@@ -31,24 +31,8 @@ server <- function(input, output, session) {
   n_bands <- 5
   n_channels <- length(channels)
   
-  coh_colormap<-array(dim = dim(mean_coherence))
-  for (b in 1:5) {
-    for (c1 in 1:30){
-      for (c2 in 1:30) {
-        if (c1==c2){
-          coh_colormap[b,c1,c2]='green'
-        }else if((mean_coherence[b,c1,c2])>0.75){
-          coh_colormap[b,c1,c2]='red'
-        }else if((mean_coherence[b,c1,c2])>0.5){
-          coh_colormap[b,c1,c2]='pink'
-        }else if(mean_coherence[b,c1,c2]>0.25){
-          coh_colormap[b,c1,c2]='mistyrose'
-        }else{
-          coh_colormap[b,c1,c2]='white'
-        }
-      }
-    }
-  }
+  #removed coh_color map
+
   
   output$eegPlot <- renderPlot({
     band_index <- switch(input$band, 
@@ -63,30 +47,8 @@ server <- function(input, output, session) {
     
     # Set the colors for each channel
     col.point <- coh_colormap[band_index, ref_ch, ]
-    electrodes <- data.frame(
-      name = c("FP1", "FP2",
-               "F7", "F3", "FZ", "F4", "F8", 
-               "FT7", "FC3", "FCZ", "FC4", "FT8", 
-               "T3", "C3", "CZ", "C4", "T4", 
-               "TP7", "CP3", "CPZ", "CP4", "TP8", 
-               "T5", "P3", "PZ", "P4", "T6", 
-               "O1", "OZ", "O2"),
-      x = c(-3, 3,
-            -6.2, -3, 0, 3, 6.2,
-            -7, -3, 0, 3, 7,
-            -8, -3, 0, 3, 8,
-            -8, -3, 0, 3, 8,
-            -7, -3, 0, 3, 7,  
-            -3.5, 0, 3.5),
-      y = c(9.5, 9.5,
-            6.5, 6.5, 6.5, 6.5, 6.5,
-            3.5, 3, 3.5, 3, 3.5,
-            0, 0, 0, 0, 0,
-            -3.5, -3, -3.5, -3, -3.5,
-            -6, -6.5, -6.5, -6.5,-6,
-            -9.5, -10.5, -9.5) # Occipitals
-    )
-    
+
+    #removed electrodes
     eegcap2d(channels,col.point = col.point)
 
     #text(electrodes$x, electrodes$y, labels = channels, pos = 3, cex = 0.7)
